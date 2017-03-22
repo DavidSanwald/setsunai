22c08480d2582c280b895b3f7af515d02172e9e4
David Sanwald <DavidSanwald@users.noreply.github.com>
2017-03-22 17:26:48 +0100

    Yay

--- src/middleware/ticker.js
+++ src/middleware/ticker.js
@@ -1,8 +1,9 @@
 // @flow
+import Rx from 'rxjs/Rx';
 import {advanceTick} from '../actions'
 import {song} from '../parameters/audioparams'
 
-export default function ticker(store) {
+export default function ticker(actualStore) {
     this.audioContext = new AudioContext();
     this.futureTickTime = this.audioContext.currentTime
     this.counter = 1
@@ -10,15 +11,19 @@ export default function ticker(store) {
     this.secondsPerBeat = (60 / this.tempo)
     this.counterTimeValue = this.secondsPerBeat / 2
 
-    this.playTick = (store) => {
-        store.dispatch(advanceTick())
+    this.playTick = () => {
+        actualStore.dispatch(advanceTick())
         this.counter += 1;
         this.futureTickTime += this.counterTimeValue
+        const bar = Math.floor(this.counter / 16) + 1
+        const beat = Math.floor(this.counter % 16 / 4) + 1
+        const s = this.counter % 4 + 1
+        console.log('Bar: ' + bar + '\nBeat: ' + beat + '\nStep: '+ s)
     }
 
     this.scheduler = () => {
         if (this.futureTickTime < this.audioContext.currentTime + 0.0001) {
-            this.playTick(store);
+            this.playTick(actualStore);
         }
         window.setTimeout(this.scheduler, 0);
     }
--- src/selectors/playingpadsSelector.js
+++
@@ -1,8 +0,0 @@
-import {instruments, song} from '../parameters/audioparams'
-
-export const getPlayingPads = (activeColumn, activePads) => {
-    const instrumentDurations = instruments.map(instrument => instrument.duration)
-    const activeColumnNumbers = instrumentDurations.map(duration => ((tick / duration) % song.barLength))
-    const activeColumns = activeColumnNumbers.map(column=> Math.floor(column))
-    return activeColumns
-}
